e15c6c57221ef9bfc0afc0f6c3b6a0d08536652c
Abhijeet Ranjan <velocitybytecode@gmail.com>
2022-11-08 14:27:10 +0530

    m

--- blockchain/nft_market/contracts/NFTMarket.sol
+++ blockchain/nft_market/contracts/NFTMarket.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.15;
 
 // Uncomment this line to use console.log
-import "../node_modules/hardhat/console.sol";
+// import "../node_modules/hardhat/console.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFTMarket {
-    
+contract NFTMarket is ERC721URIStorage {
+    constructor() ERC721("Cuties", "KYUT") {}
 }
