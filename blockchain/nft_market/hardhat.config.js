855954ab6fc3d4c4fabbbf1b5918457dbca5b0c3
Abhijeet Ranjan <velocitybytecode@gmail.com>
2022-11-25 21:16:17 +0530

    fix optimizer syntax

--- blockchain/nft_market/hardhat.config.js
+++ blockchain/nft_market/hardhat.config.js
@@ -3,17 +3,19 @@ const keys = require("../../keys.json");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100000,
+      },
+    },
+  },
   networks: {
     goerli: {
       url: keys.INFURA_GOERLI_URL,
       accounts: [`0x${keys.PRIVATE_KEY}`],
-    },
-  },
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 100000,
     },
   },
 };
